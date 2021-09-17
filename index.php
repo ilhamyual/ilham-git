b78508bd13f0e5157d4757756884f0915231db0b
ilhamyual <ilhamyual22@gmail.com>
2021-09-17 13:55:11 +0700

    isi coding

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>belajar git - ilham-git</title>
+  </head>
+  <body>
+    <p>halo semua , aku sedang belajar git</p>
+  </body>
+</html>
