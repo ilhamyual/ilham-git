a4b57d69de1ed0c3887223c99b5e6cdc7ac67b0f
ilhamyual <ilhamyual22@gmail.com>
2021-09-27 19:01:34 +0700

    third project

---
+++ RelativeLayout/app/src/main/java/com/example/relativelayout/RelativeLayout.java
@@ -0,0 +1,55 @@
+package com.example.relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Calendar;
+
+public class RelativeLayout extends AppCompatActivity {
+    DatePickerDialog picker;
+    EditText etext;
+    Button btnGet;
+    TextView tvw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvw=(TextView) findViewById(R.id.textView1);
+        etext=(EditText) findViewById(R.id.editText1);
+        etext.setInputType(InputType.TYPE_NULL);
+        etext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(RelativeLayout.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                                  int dayOfMonth) {
+                                etext.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+        btnGet = (Button) findViewById(R.id.button1);
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvw.setText("Selected Date: " + etext.getText());
+            }
+        });
+    }
+}
